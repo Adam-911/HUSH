3d934459714126c03b84a39a7cbed471d239e1d2
Adam-911 <40604826+Adam-911@users.noreply.github.com>
2019-02-09 15:09:41 +0600

    dsf
    sdf

---
+++ Assets/HUSH/Scripts/OpenStartLevel.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class OpenStartLevel : MonoBehaviour {
+
+    // Метод загрузки стартовой сцены  
+    public void startGame() {
+        SceneManager.LoadScene("Start");
+    }
+    
+    // Метод закрывает приложение
+    public void quitGame() {
+        Application.Quit();
+    }
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
